1ed826fad4d2faa215ab5481fe7d6771eae3bff4
Afeez Oluwashina Adeboye <afeezadeboyee@gmail.com>
2023-02-21 09:05:30 +0000

    updated the notificationfeed.js file

--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -10,7 +10,7 @@ import ReplyForm from '../components/ReplyForm';
 // [TODO] Authenication
 import Cookies from 'js-cookie'
 
-export default function HomeFeedPage() {
+export default function NotificationsFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState(false);
   const [poppedReply, setPoppedReply] = React.useState(false);
