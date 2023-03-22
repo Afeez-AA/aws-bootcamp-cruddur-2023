1ddc00717820ee4aa74cfa8d055f01d28f1ede54
Afeez Oluwashina Adeboye <afeezadeboyee@gmail.com>
2023-03-22 18:33:23 +0000

    impletemented all live class features for week 4

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,13 @@
+-- this file was manually created
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Afeez Adeboye', 'afeezadeboye' ,'MOCK'),
+  ('Fauziyahh Onijogbon', 'Fauziyahh Onijogbon' ,'MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'afeezadeboye' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
