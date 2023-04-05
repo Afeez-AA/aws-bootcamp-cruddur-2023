bdb1b30a514cc66c90c3d8f91b7c710d4bc20ee0
Afeez Oluwashina Adeboye <afeezadeboyee@gmail.com>
2023-04-05 14:29:59 +0000

    fixed the seed ddb

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
--- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+-- this file was manually created (because we might auto generate it in future)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Afeez Adeboye', 'afeezadeboye' ,'MOCK'),
-  ('Jamal Adeboye', 'jamaladeboye' ,'MOCK');
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Londo Londo','Londo@londo.co' , 'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'afeezadeboye' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
