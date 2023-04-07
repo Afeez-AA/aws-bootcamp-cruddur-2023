fcc3d0f7d3a1c4c72365a5b55f77a6dbc309d8f1
Afeez Oluwashina Adeboye <afeezadeboyee@gmail.com>
2023-04-07 10:13:57 +0000

    gave each script executatable permissions

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -12,3 +12,17 @@ VALUES
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
+-- -- this file was manually created (because we might auto generate it in future)
+-- INSERT INTO public.users (display_name, email, handle, cognito_user_id)
+-- VALUES
+--   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+--   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+--   ('Londo Londo','Londo@londo.co' , 'londo' ,'MOCK');
+
+-- INSERT INTO public.activities (user_uuid, message, expires_at)
+-- VALUES
+--   (
+--     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+--     'This was imported as seed data!',
+--     current_timestamp + interval '10 day'
+--   )
