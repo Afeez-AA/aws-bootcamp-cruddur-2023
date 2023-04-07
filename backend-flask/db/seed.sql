ec71d6b39c593076e0a13937b336b48a3cfebab0
Afeez Oluwashina Adeboye <afeezadeboyee@gmail.com>
2023-04-07 12:37:25 +0000

    updates the cognito user info

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created (because we might auto generate it in future)
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Afeez Adeboye','aa@gmail.com' , 'afeezmock' ,'MOCK'),
+  ('Afeez Adeboye','afeezadeboyee@gmail.com' , 'A.A' ,'MOCK'),
   ('Jamal Adeboye','ja@test.co' , 'jamalmock' ,'MOCK'),
-  ('Zainab Adeboye','za@test.pro' , 'zainabmock' ,'MOCK');
+  ('Zainab Adeboye','oluwashinafeez@yahoo.com' , 'zainabmock' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'afeezmock' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'A.A' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
