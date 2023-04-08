2d39b99172a4fb3291f4da2e2b66211ffa3bcf96
Afeez Adeboye <afeezadeboyee@gmail.com>
2023-04-08 22:57:24 +0100

    Revert "customized the mock users" This is reverting my code to the previous task
    This reverts commit 4213351ac28530ba33e67882e9de82e8a4766f2a.

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,9 +2,15 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
 <<<<<<< HEAD
+<<<<<<< HEAD
   ('Afeez Adeboye','afeezadeboyee@gmail.com' , 'A.A' ,'MOCK'),
   ('Jamal Adeboye','ja@test.co' , 'jamalmock' ,'MOCK'),
   ('Zainab Adeboye','oluwashinafeez@yahoo.com' , 'zainabmock' ,'MOCK');
+=======
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Londo Londo','Londo@londo.co' , 'londo' ,'MOCK');
+>>>>>>> parent of 4213351 (customized the mock users)
 =======
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
@@ -15,7 +21,11 @@ INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
 <<<<<<< HEAD
+<<<<<<< HEAD
     (SELECT uuid from public.users WHERE users.handle = 'A.A' LIMIT 1),
+=======
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+>>>>>>> parent of 4213351 (customized the mock users)
 =======
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
 >>>>>>> parent of 4213351 (customized the mock users)
