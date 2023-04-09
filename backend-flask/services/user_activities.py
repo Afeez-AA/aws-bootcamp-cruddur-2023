502638747b31c26369015c2f8fb8ba3b9a24b5b9
Afeez Adeboye <afeezadeboyee@gmail.com>
2023-04-09 14:35:11 +0100

    updated code

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
     model = {
@@ -14,7 +16,7 @@ class UserActivities:
       now = datetime.now()
       results = [{
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
+        'handle':  'A.A',
         'message': 'Cloud is fun!',
         'created_at': (now - timedelta(days=1)).isoformat(),
         'expires_at': (now + timedelta(days=31)).isoformat()
